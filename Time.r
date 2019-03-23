d4969c0ff382c1b10e2ff3959d0fd52574b616fc
Ygor Silva <ygorcs@hisston.com>
2019-03-23 15:03:02 -0300

    Citibike db cloned

---
+++ Time.r
@@ -0,0 +1,3 @@
+hours <- function(timeString) {
+  
+}
---
+++ readData.r
@@ -0,0 +1,10 @@
+readCSVDirectory <- function(dir) {
+  files <- list.files(dir)
+  data <- read.csv(paste(dir, '/', files[1], sep=""))
+  files <- files[seq(2:length(files))]
+  for (file in files) {
+    data2 <- read.csv(paste(dir, '/', file, sep=""))
+    merge(data, data2)
+  }
+  return(data)
+}
