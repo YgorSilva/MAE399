779e50308e0bb03a46342fe7ed6b0fbfa02e9e85
Ygor Silva <ygorcs@hisston.com>
2019-05-22 20:59:42 -0300

    Lista 5 prontinha

--- lista4/lista4.r
+++ lista4/lista4.r
@@ -1,3 +1,18 @@
+factorial <- function (n) {
+  f <- 1
+  if (n > 0) {
+    for (i in seq(n, 1)) {
+      f <- f*i
+    }
+  }
+  return(f)
+}
+
+combinatorial <- function (n, r) {
+  return(factorial(n)/(factorial(r)*factorial(n - r)))
+}
+
+
 randomWalk <- function (n, p = 0.5, dim = 1) {
   rwAxis <- c()
   for (i in seq(1, dim)) {
@@ -61,12 +76,20 @@ cutOffValues <- function (ns, sims, p) {
 
 countLidershipChanges <- function(randomWalk) {
   s <- 0
-  for (i in seq(2, length(randomWalk) - 1)) {
+  for (i in seq(2, length(randomWalk) - 1, 2)) {
     if (randomWalk[i-1] + randomWalk[i + 1] == 0) {
       s <- s + 1
     }
   }
   return(s)
+}
+
+lidershipChangesProbability <- function (N) {
+  probs <- c()
+  for (r in seq(0, N)) {
+    probs <- c(probs, combinatorial(N, (N - 1)/2 - r) / 2**(N - 1))
+  }
+  return(probs)
 }
 
 lidershipChanges <- function(randomWalks) {
@@ -75,6 +98,14 @@ lidershipChanges <- function(randomWalks) {
     s <- c(s, countLidershipChanges(randomWalks[,1,i]))
   }
   return(s)
+}
+
+last0Probability <- function (N) {
+  probs <- c()
+  for (k in seq(0, N, 2)) {
+    probs <- c(probs, combinatorial(k, k/2) * combinatorial(N - k, (N - k)/2) / 2**N)
+  }
+  return(probs)
 }
 
 findLast0 <- function(randomWalk) {
@@ -92,6 +123,11 @@ last0Ocurrence <- function(randomWalks) {
     l0s <- c(l0s, findLast0(randomWalks[,1,i]))
   }
   return(l0s)
+}
+
+gamblersRuinProbability <- function (p, c, d, x) {
+  q <- 1 - p
+  return ((1 - (q/p)**(x - c)) / (1 - (q/p)**(d - c)))
 }
 
 gamblersRuin <- function (x0, c, d, p = 0.5) {
@@ -113,31 +149,53 @@ runNgamblersRuin <- function (N, x0, c, d, p = 0.5) {
   }
   return(ruins)
 }
-#
+
 #png("changes.png")
-#p <- runif(1)
-#randomWalks <- runNRandomWalks(1000, 101, p)
-#changes <- lidershipChanges(randomWalks)
+randomWalks <- runNRandomWalks(1000, 101)
+changes <- lidershipChanges(randomWalks)
+changesProps <- c()
+for (n in seq(0,101)) {
+  changesProps <- c(changesProps, sum(changes == n) / 1000)
+}
+changesProbs <- lidershipChangesProbability(101)
+table1 <- data.frame(changesProps, changesProbs)
+write.csv(table1, file = "q1a.csv")
+
 #hist(changes, n=max(changes), 
-#  main = paste("Trocas de Liderança (p = ", p, ")", sep=""),
+#  main = "Passeio Aleatório Simples Simétrico",
 #  xlab = "Quantidade de Trocas de Liderança",
 #  ylab = "Frequência")
 #dev.off()
 #
 #png("lastZeros.png")
-#lastZeros <- last0Ocurrence(randomWalks)
+
+randomWalks <- runNRandomWalks(1000, 100)
+lastZeros <- last0Ocurrence(randomWalks)
+lastZerosProps <- c()
+for (n in seq(0,100, 2)) {
+  lastZerosProps <- c(lastZerosProps, sum(lastZeros == n) / 1000)
+}
+lastZerosProbs <- last0Probability(100)
+table2 <- data.frame(lastZerosProps, lastZerosProbs)
+write.csv(table2, file = "q1b.csv")
+
 #hist(lastZeros, n=max(lastZeros), 
-#  main = paste("Últimas Visitas a Origem (p = ", p, ")", sep=""),
+#  main = "Últimas Visitas a Origem em Um P.A.S.S.",
 #  xlab = "Última visita",
 #  ylab = "Frequência")
-#dev.off()
+#  dev.off()
 #
 #png("gamblersRuin.png")
-#wins <- c()
-#for (x0 in seq(10, 40, 10)) {
-#  ruins <- runNgamblersRuin(1000, x0, 0, 50, 18/38)
-#  wins <- c(wins, sum(ruins))
-#}
+wins <- c()
+winsProbs <- c()
+for (x0 in seq(10, 40, 10)) {
+  ruins <- runNgamblersRuin(1000, x0, 0, 50, 18/38)
+  wins <- c(wins, sum(ruins)/1000)
+  winsProbs <- c(winsProbs, gamblersRuinProbability(18/38, 0, 50, x0))
+}
+table3 <- data.frame(wins, winsProbs)
+write.csv(table3, file = "q2a.csv")
+
 #plot(x = seq(10,40,10), y = wins/1000, 
 #  main = "Ruina do Jogador",
 #  xlab = "x0",
@@ -145,20 +203,30 @@ runNgamblersRuin <- function (N, x0, c, d, p = 0.5) {
 #dev.off()
 #
 #png("symmetricGamblersRuin.png")
-#wins <- c()
-#for (x0 in seq(10, 40, 10)) {
-#  ruins <- runNgamblersRuin(1000, x0, 0, 50)
-#  wins <- c(wins, sum(ruins))
-#}
+winsProps <- c()
+winsProbs <- c()
+for (x0 in seq(10, 40, 10)) {
+  ruins <- runNgamblersRuin(1000, x0, 0, 50)
+  winsProps <- c(winsProps, sum(ruins)/1000)
+}
+table4 <- data.frame(winsProps)
+write.csv(table4, file = "q2b.csv")
 #plot(x = seq(10,40,10), y = wins/1000, 
 #  main = "Ruina do Jogador - Passeio Simétrico",
 #  xlab = "x0",
 #  ylab = "Probabilidades (p)")
 #dev.off()
 
-#randomWalks <- runNRandomWalks(1000, 10000, p = 0.5, dim = 2)
+randomWalks <- runNRandomWalks(1000, 10000, p = 0.5, dim = 2)
 expectedSn <- c()
 for (i in seq(1000, 10000, 1000)) {
   meanDist <- mean(sqrt(randomWalks[i,1,]**2 + randomWalks[i,2,]**2))
   expectedSn <- c(expectedSn, meanDist)
 }
+write.csv(data.frame(expectedSn), file = "q3.csv")
+png('q3.png')
+plot(x = seq(1000, 10000, 1000), y = expectedSn, 
+  main = "Passeio Aleatório Simétrico no Plano",
+  xlab = "N (n-ésimo passo)",
+  ylab = "Distância média")
+dev.off()
